751170e4f0051f67d71b8e0f6d3a486ac7ace743
Raphael Aciardi <109046764+raphael-aciardi@users.noreply.github.com>
2026-03-21 19:23:19 -0300

    feat: initial commit

---
+++ vite.config.ts
@@ -0,0 +1,29 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from "path"
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    react({
+      babel: {
+        plugins: [
+          [
+            'module-resolver',
+            {
+              root: ['./src'],
+              alias: {
+                '@': './src',
+              },
+            },
+          ],
+        ],
+      },
+    }),
+  ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+})
